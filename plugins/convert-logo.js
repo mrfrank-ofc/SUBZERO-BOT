73824a8dd6deb10ff1ec89eaa7aafe649045b9af
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-03-29 19:25:56 +0200

    Update convert-logo.js

--- plugins/convert-logo.js
+++ plugins/convert-logo.js
@@ -233,7 +233,7 @@ cmd({
 }, 
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        let dec = `*\` 🔮 SUBZERO LOGO MENU 🔮\`*
+        let dec = `\`🔮 SUBZERO LOGO MENU 🔮\`
 ╭───❍
 ‎*├⬡ .ɴᴇᴏɴʟɪɢʜᴛ*
 ‎*├⬡ .ʙʟᴀᴄᴋᴘɪɴᴋ*
